3b6edc00c060cbd30ed64c76399445d1d4c3bf02
kaleemmoiz123341 <moizkaleem51@gmail.com>
2025-04-29 21:37:08 +0500

    auth0 done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from "./routes/MyUserRoutes"
+import myUserRoute from "./routes/MyUserRoutes";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/my/user", myUserRoute)
+app.use("/api/my/user", myUserRoute);
 
 app.get("/test", async (req: Request, res: Response) => {
   res.json({ message: "HELLO!" });
--- backend/src/routes/MyUserRoutes.ts
+++ backend/src/routes/MyUserRoutes.ts
@@ -1,8 +1,9 @@
 import express, { RequestHandler } from "express";
 import MyUserController from "../controllers/MyUserController";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
-router.post("/", MyUserController.createCurrentUser as RequestHandler);
+router.post("/", jwtCheck, MyUserController.createCurrentUser as RequestHandler);
 
 export default router;
